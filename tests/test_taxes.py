a2eab98f980d1def1f525ae773f5bcd66c7144d9
Olga Yakimova <oa.yakimova.1969@gmail.com>
2025-03-12 15:26:44 +0300

    добавили задачу 2

--- tests/test_taxes.py
+++ tests/test_taxes.py
@@ -1,5 +1,6 @@
 import pytest
 from src.taxes import calculate_taxes
+from src.taxes import calculate_tax
 
 
 @pytest.fixture
@@ -23,3 +24,24 @@ def test_calculate_taxes_invalid_tax_rate(prices):
 def test_calculate_taxes_invalid_prices():
     with pytest.raises(ValueError):
         calculate_taxes([0, -1], tax_rate=10)
+
+
+@pytest.mark.parametrize("price, tax_rate, expected", [(100, 10, 110),
+                                                       (50, 5, 52.5)])
+def test_calculate_tax(price, tax_rate, expected):
+    assert calculate_tax(price, tax_rate) == expected
+
+
+def test_calculate_tax_invalid_price():
+    with pytest.raises(ValueError):
+        calculate_tax(-1, 10)
+
+
+def test_calculate_tax_invalid_tax_rate_below_zero():
+    with pytest.raises(ValueError):
+        calculate_tax(100, -1)
+
+
+def test_calculate_tax_invalid_tax_rate_after_100():
+    with pytest.raises(ValueError):
+        calculate_tax(100, 1000)
